73f8e608780067cba7a06c3a471c335a154ad50d
Himanshi Khatwani <67739958+himanshi-png@users.noreply.github.com>
2021-05-16 23:36:43 +0530

    Update Prediction Using Unsupervised ML.py

--- Prediction Using Unsupervised ML.py
+++ Prediction Using Unsupervised ML.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
 # Downloading the datset
-
-
-# In[2]:
-
-
 # importing the libraries
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -17,74 +8,24 @@ import numpy as np
 import seaborn as sns
 from sklearn import datasets
 
-
-# In[3]:
-
-
 # import the dataset
 iris_raw=pd.read_csv("Iris.csv")
 
-
-# In[4]:
-
-
 iris_raw
-
-
-# In[5]:
-
-
 # summary of data
 iris_raw.describe()
-
-
-# In[6]:
-
-
 iris_raw.isnull()
 
-
-# In[7]:
-
-
 # Finding corelation between variables
-
-
-# In[8]:
-
-
 corr = iris_raw.corr()
 corr
 
-
-# In[9]:
-
-
 sns.pairplot(iris_raw,hue='Species');
-
-
-# In[10]:
-
-
 # Pair plot
-
-
-# In[11]:
-
-
 sns.set()
 sns.heatmap(iris_raw.corr(),annot= True, cmap="pink");
 
-
-# In[12]:
-
-
 # Find the optimum number of clusters for K-Means and then value of K
-
-
-# In[14]:
-
-
 # Defining "x"
 x = iris_raw.iloc[:, [0, 1, 2, 3]].values
 
@@ -107,30 +48,11 @@ plt.xlabel('Number of clusters')
 plt.ylabel('WCSS') # Within cluster sum of squares
 plt.show();
 
-
-# In[15]:
-
-
 # Creating the kmeans classifier
 kmeans = KMeans(n_clusters=5, init ='k-means++', max_iter=300, n_init=10,random_state=0 )
-
-
-# In[16]:
-
-
 y_kmeans = kmeans.fit_predict(x)
 
-
-# In[17]:
-
-
 # Visualising the dataset
-
-
-# In[18]:
-
-
-
 plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], 
             s = 100, c = 'red', label = 'Iris-setosa');
 plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], 
@@ -143,10 +65,4 @@ plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:,1],
             s = 100, c = 'yellow', label = 'Centroids');
 
 plt.legend();
-
-
-# In[ ]:
-
-
-
 
